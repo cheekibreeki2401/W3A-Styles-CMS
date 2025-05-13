a0fa84e6e1717dd09b162f81bf89448550f73618
Kyle <kaeverett10@iinet.net.au>
2025-05-13 17:46:43 +1000

    Add grid based css

--- U3A-Style/functions.php
+++ U3A-Style/functions.php
@@ -7,6 +7,7 @@ add_action( 'wp_enqueue_scripts', 'U3A_styles');
 function childtheme_enqueue_styles() {
     wp_enqueue_style( 'child-theme-fonts', 'https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;700&display=swap', false );
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+    wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', ['parent-style'] );
 }
 add_action( 'wp_enqueue_scripts', 'childtheme_enqueue_styles' );
 

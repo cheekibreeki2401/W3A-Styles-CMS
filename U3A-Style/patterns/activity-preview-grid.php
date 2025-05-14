acd9819ecd3132edaedade5ecc65991f855c7501
Kyle <kaeverett10@iinet.net.au>
2025-05-14 16:12:15 +1000

    Update visual aspects of pages to better suit

--- U3A-Style/patterns/activity-preview-grid.php
+++ U3A-Style/patterns/activity-preview-grid.php
@@ -7,7 +7,7 @@
  * Keywords: activities, courses, grid
  */
 ?>
-<!-- wp:group {"layout":{"type":"constrained","wideSize":"1200px"}} -->
+<!-- wp:group {"layout":{"type":"constrained","wideSize":"1400px"}} -->
 <div class="wp-block-group has-cool-blue-background-color has-background">
   <!-- wp:heading {"textAlign":"center"} -->
   <h2 class="wp-block-heading has-text-align-center">Explore Our Most Popular Activities</h2>
@@ -17,10 +17,11 @@
   <div class="wp-block-columns has-3-columns">
 
     <!-- wp:column -->
-    <div class="wp-block-column">
-      <!-- wp:image {"sizeSlug":"medium","linkDestination":"custom","href":"/activities/philosophy-classes"} -->
+    <div class="wp-block-column activity-column-link">
+     <a href="/activities/philosophy-classes">
+      <!-- wp:image {"sizeSlug":"medium"} -->
       <figure class="wp-block-image size-medium">
-        <a href="/activities/philosophy-classes"><img src="<?php echo get_stylesheet_directory_uri(). '/assets/philosophy%20group.jpeg'; ?> " alt="philosophy"></a>
+       <img src="<?php echo get_stylesheet_directory_uri(). '/assets/philosophy%20group.jpeg'; ?> " alt="philosophy">
       </figure>
       <!-- /wp:image -->
 
@@ -31,14 +32,16 @@
       <!-- wp:paragraph -->
       <p>Learn about the core of human nature in a friendly environment with casual debates and exploration of our minds and personal philosophy</p>
       <!-- /wp:paragraph -->
+      </a>
     </div>
     <!-- /wp:column -->
 
     <!-- wp:column -->
-    <div class="wp-block-column">
-      <!-- wp:image {"sizeSlug":"medium","linkDestination":"custom","href":"/activities/dancing-class"} -->
+    <div class="wp-block-column activity-column-link">
+    <a href="/activities/dancing-class">
+      <!-- wp:image {"sizeSlug":"medium"} -->
       <figure class="wp-block-image size-medium">
-        <a href="/activities/dancing-class"><img src="<?php echo get_stylesheet_directory_uri(). '/assets/dancing.JPG'; ?> " alt="dancing"></a>
+        <img src="<?php echo get_stylesheet_directory_uri(). '/assets/dancing.JPG'; ?> " alt="dancing">
       </figure>
       <!-- /wp:image -->
 
@@ -49,14 +52,16 @@
       <!-- wp:paragraph -->
       <p>Break out those moves on the dance floor! Dancing up a storm, our dance classes make sure that you are ready for that new ball or teach you some good ol' fashion folk dances to classic tunes.</p>
       <!-- /wp:paragraph -->
+      </a>
     </div>
     <!-- /wp:column -->
 
     <!-- wp:column -->
-    <div class="wp-block-column">
-      <!-- wp:image {"sizeSlug":"medium","linkDestination":"custom","href":"/activities/language-cafe"} -->
+    <div class="wp-block-column activity-column-link">
+    <a href="/activities/language-cafe">
+      <!-- wp:image {"sizeSlug":"medium"} -->
       <figure class="wp-block-image size-medium">
-        <a href="/activities/language-cafe"><img src="<?php echo get_stylesheet_directory_uri() . '/assets/FrenchGroup4.JPG'; ?>" alt="Language café"></a>
+        <img src="<?php echo get_stylesheet_directory_uri() . '/assets/FrenchGroup4.JPG'; ?>" alt="Language café">
       </figure>
       <!-- /wp:image -->
 
@@ -67,6 +72,7 @@
       <!-- wp:paragraph -->
       <p>Practice a wide variety of languages including French, Spanish and German while over a good cuppa.</p>
       <!-- /wp:paragraph -->
+      </a>
     </div>
     <!-- /wp:column -->
 
